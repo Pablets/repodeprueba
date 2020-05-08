a48f5e2636094aad7cf20cd8d69dc3fc772b27d5
Pablets <godoypablom@gmail.com>
2020-05-08 11:39:16 -0300

    animación bodymovin

---
+++ scripts/index.js
@@ -0,0 +1,7 @@
+lottie.loadAnimation({
+    container: document.getElementById('bm'), // the dom element that will contain the animation
+    renderer: 'html',
+    loop: true,
+    autoplay: true,
+    path: 'data.json' // the path to the animation json
+  });
